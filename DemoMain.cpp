146a1cfac69e4e774df12d92561dc8975b444419
J0rdanN89 <u24664155@tuks.co.za>
2025-08-01 18:22:19 +0200

    Just to set up main

---
+++ DemoMain.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main()
+{
+    std::cout<<"Welcome to my youtube channel"<<std::endl;
+
+    return 0;
+}
---
+++ TestingMain.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main()
+{
+    std::cout<<"Just so we can have branches and stuff"<<std::endl;
+
+    return 0;
+}
